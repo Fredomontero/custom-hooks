2002cd480d5ea3047eb7495f9032bd0938e44e2f
cmonteromtz <christianmontero08@gmail.com>
2021-03-11 17:06:00 -0800

    Add functionality for useIndexDB

--- src/useIndexDB.js
+++ src/useIndexDB.js
@@ -5,34 +5,35 @@ export const useIndexDB = () => {
   const [ data, setData ] = useState([]);
   let database, objectStore;
 
-  const initDB = async () => {
-
-    console.log("Inside initDB")
-    const request = await window.indexedDB.open('database', 1);
-
-    request.onerror = () => console.error("ERROR", request.error);
-    
-    request.onupgradeneeded = (event) => {
-      console.log("CREATE");
-      database = request.result;
-      console.log("DB: ", database);
-      objectStore = database.createObjectStore("todos", {keyPath: "id"});
-    };
-    
-    request.onsuccess = () => {
-      console.log("OPEN");
-      database = request.result;
-      console.log("DB inside onSuccess: ", database);
-      loadData();
-    };
-
-    return database;
-    
+  const initDB = () => {
+    return new Promise((resolve) => {
+      console.log("Inside initDB")
+      const request = window.indexedDB.open('database', 1);
+      
+      request.onerror = () => console.error("ERROR", request.error);
+      
+      request.onupgradeneeded = (event) => {
+        console.log("CREATE");
+        database = request.result;
+        console.log("DB: ", database);
+        objectStore = database.createObjectStore("todos", {keyPath: "id"});
+      };
+      
+      request.onsuccess = () => {
+        console.log("OPEN");
+        database = request.result;
+        console.log("DB inside onSuccess: ", database);
+        resolve(database);
+      };
+    });
   };
 
   useEffect(() => {
-    initDB();
-    console.log("DB inside UseEffect: ", database);
+    initDB()
+    .then(async db => {
+      console.log("Database inside useEffect :", db);
+      // loadData();
+    })
   }, []);
 
   //1) SAVE INTO DB
@@ -44,8 +45,8 @@ export const useIndexDB = () => {
     let todo = { id:  `data-${data.length}`, description: value };
     const request = objectStore.add(todo);
     // window.sessionStorage.setItem(`data-${data.length}`, value);
-    // let tempData = [ ...data, {id: `data-${data.length}`, description: value} ];
-    // setData(tempData);
+    let tempData = [ ...data, {id: `data-${data.length}`, description: value} ];
+    setData(tempData);
   };
 
   //2) DELETE FROM DB
@@ -59,17 +60,33 @@ export const useIndexDB = () => {
 
   //3) RETRIEVE FROM DB
   const loadData = () => {
-    console.log("Loading data");
+    console.log("LOADING DATA");
+    console.log("DB: ", database);
     let tempData = [];
-    for (const key in window.sessionStorage) {
-      if(key.includes('data'))
-        // console.log(`${key}: ${window.localStorage[key]}`);
+    const transaction = database.transaction(['todos'], 'readonly');
+    const objectStore = transaction.objectStore('todos');
+    const request = objectStore.openCursor();
+
+    request.onsuccess = (e) => {
+      const cursor = e.target.result;
+      if(cursor){
+        console.log(cursor.value);
         tempData.push({
-          id: key,
-          description: window.sessionStorage[key]
+          id: cursor.value.id,
+          description: cursor.value.description
         });
+        cursor.continue();
+      }else{
+        console.log("No more data");
         setData(tempData);
+      }
     }
+    // let tempData = [];
+    // for (const key in window.sessionStorage) {
+    //   if(key.includes('data'))
+    //     // console.log(`${key}: ${window.localStorage[key]}`);
+    
+    // }
   };
 
   return {
